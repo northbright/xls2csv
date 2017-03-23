8e7ec71e75fcdb754a3a99dfc8e2def40e26d0bc
Frank Xu <frank.xu@northbright.com>
2017-03-23 02:27:47 -0400

    Implemented main() for xls2csv program.

--- src/main.c
+++ src/main.c
@@ -6,48 +6,105 @@
 
 #include "xls2csv.h"
 
-const char *usage = "Usage: xls2csv [-x XLS file] [-c CSV file] [-s sheet ID]";
+const char *usage = "Usage: xls2csv [XLS file] [-s sheet ID(0-based)] [-o CSV File]";
+const char *example = "Example: xls2csv my.xls -s 2 -o my.csv";
 
 int main(int argc, char *argv[]) {
     int oc;
     char ec;
-    char *opt_arg;
-    char *xls_file = NULL;
-    char *csv_file = NULL;
+    char *optArg;
+    char *xlsFile = NULL;
+    char *csvFile = NULL;
     int nSheetId = 0;
-
+    int fInvalidArgs = 0;
+    int nRet = EXIT_FAILURE;
+    char *buf = NULL;
+    FILE *f = NULL;
     opterr = 0;
-
-    while((oc = getopt(argc, argv, ":x:c:s:")) != -1) {
+    
+    // Use getopt() to parse args.
+    while((oc = getopt(argc, argv, ":s:o:")) != -1) {
         switch(oc) {
-	    case 'x':
-                xls_file = optarg;
-		break;
- 
-            case 'c':
-                csv_file = optarg;
+            case 's':
+                nSheetId = atoi(optarg);
                 break;
 
-            case 's':
-		nSheetId = atoi(optarg);
-		break;
+            case 'o':
+                csvFile = optarg;
+                break;
 
 	    case '?':
+	        fInvalidArgs = 1;
 	        ec = (char)optopt;
 		printf("Invalid arg: %c.\n", ec);
-		break;
+		goto end;
 
 	    case ':':
+		fInvalidArgs = 1;
 	        printf("Missing argument.\n");
-		break;
+                goto end;
 
 	    default:
+		fInvalidArgs = 1;
 	        printf("Unknown argument.\n");
+		goto end;
         }
     }
 
-    printf("xls_file: %s, csv_file: %s, nSheetId: %d\n", xls_file, csv_file, nSheetId);
+    // Check if XLS file is specified.
+    // After getopt() exited, optind is where non-opt arg begins.
+    if (optind != argc - 1) {
+	fInvalidArgs = 1;
+        printf("Please specify one(and only one) XLS file.\n");
+	goto end;
+    }
+
+    // Get XLS file name.
+    xlsFile = argv[optind];
+
+    // Call xls2csv() to get CSV string.
+    buf = xls2csv(xlsFile, nSheetId);
+    if (buf == NULL) {
+	goto end;
+    }
+
+    // Just output CSV string to stdout if no CSV file arg specified.
+    if (csvFile == NULL) {
+	printf(buf);
+	nRet = EXIT_SUCCESS;
+	goto end;
+    }
+
+    // Write converted CSV string to output CSV file.
+    if (strlen(csvFile) == 0) {
+	printf("CSV file name is empty.\n");
+	goto end;
+    }
+
+    f = fopen(csvFile, "w");
+    if (f == NULL) {
+	printf("Open CSV file: %s failed.\n", csvFile);
+	goto end;
+    }
+
+    fputs(buf, f);
+
+    if (fclose(f) != 0) {
+	printf("Close CSV file: %s failed.\n", csvFile);
+	goto end;
+    }
+
+    nRet = EXIT_SUCCESS;
 
 end:
-    return;
+    if (fInvalidArgs) {
+        printf("%s\n%s\n", usage, example);
+    }
+
+    if (buf != NULL) {
+	free(buf);
+	buf = NULL;
+    }
+
+    return nRet;
 }
